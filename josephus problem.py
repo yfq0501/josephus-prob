cad31a20afa8e6e5132414ea65b24135ec9cc6be
yfq <2408116467@qq.com>
2022-07-10 15:43:24 +0800

    process

--- josephus problem.py
+++ josephus problem.py
@@ -9,3 +9,19 @@ for i in range(0,m,1):
 
 print("大家的编号为：%s"%list1)
 
+i=0
+flag=1
+while len(list1)>=n:
+    if i==len(list1):
+        i=0
+    if flag==n :
+        list1.pop(i)
+        flag=1
+        print("目前存活的数为：%s"%list1)
+        continue  
+    
+        
+    else:
+        flag+=1
+         
+    i+=1
